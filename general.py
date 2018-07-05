869b421414e9bd2e8ee2729cf22aa8f493e0ed7a
spanchbob@gmail.com <Lkzgbnjyf777>
2018-07-05 19:36:22 +0300

    add functions set to file and file to set

--- general.py
+++ general.py
@@ -35,3 +35,20 @@ def append_to_file(path, data):
 def delete_file_contents(path):
     with open(path, 'w'):
         pass
+
+
+# read a file and convert each line to set items
+def file_to_set(file_name):
+    results  = set()
+    with open(file_name, 'rt') as file:
+        for line in file:
+            results.add(line.replace('\n', ''))
+    return results
+
+
+# iterate through a set, each item will be a new line in the file
+def set_to_file(links, file):
+    delete_file_contents(file)
+    for link in sorted(links):
+        append_to_file(file, link)
+        

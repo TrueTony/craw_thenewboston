ce76c25682270cc38e7397d27257b6bbe775bb56
TrueTony <spanchbob@gmail.com>
2018-07-11 20:47:49 +0300

    add new functions to spider

--- spider.py
+++ spider.py
@@ -19,7 +19,7 @@ class Spider:
         Spider.base_url = base_url
         Spider.domain_name = domain_name
         Spider.queue_file = Spider.project_name + '/queue.txt'
-        Spider.crawled = Spider.project_name + '/crawled.txt'
+        Spider.crawled_file = Spider.project_name + '/crawled.txt'
         self.boot()
         self.crawl_page('First spider', Spider.base_url)
 
@@ -39,3 +39,35 @@ class Spider:
             Spider.add_links_to_queue(Spider.gather_links())
             Spider.queue.remove(page_url)
             Spider.crawled.add(page_url)
+            Spider.update_files()
+            
+    @staticmethod
+    def gather_links(page_url):
+        html_string = ''
+        try:
+            responce = urlopen(page_url)
+            if responce.getheader('Content-type') == 'text/html':
+                html_bytes = responce.read()
+                html_string = html_bytes.decode('utf-8')
+            finder = LinkFinder(Spider.base_url, page_url)
+            finder.feed(html_string)
+        except:
+            print('Error: can not crawl page')
+            return set()
+        return finder.page_links()
+
+    @staticmethod
+    def add_links_to_queue(links):
+        for url in links:
+            if url in Spider.queue:
+                continue
+            if url in Spider.crawled:
+                continue
+            if Spider.domain_name is not url:
+                continue
+            Spider.queue.add(url)
+
+    @staticmethod
+    def update_files():
+        set_to_file(Spider.queue, Spider.queue_file)
+        set_to_file(Spider.crawled, Spider.crawled_file)
